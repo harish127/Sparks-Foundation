3ff8580af736ff4508fb1d3075a44526e2adeed2
harish <harishkaiwart127@gmail.com>
2021-09-04 21:26:13 -0700

    logo part text has been added

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
     <!-- navbar part start -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">Money Bank</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
