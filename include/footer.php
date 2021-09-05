5b3c1ac427a8e47ee63ac1da9f39188b8961a818
harish <harishkaiwart127@gmail.com>
2021-09-04 21:08:05 -0700

    Header and Footer files common to all pages

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+      <!-- footer part start -->
+      <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">View all Customers</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+        </ul>
+        <p class="text-center text-muted">© 2021 Money Bank, Inc</p>
+      </footer>
+      <!-- footer part end -->
