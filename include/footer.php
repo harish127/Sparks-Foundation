7d379cfad495d768358ee5570eb73269fc8d7a18
harish <harishkaiwart127@gmail.com>
2021-09-04 21:25:40 -0700

    path to links is added.

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,9 @@
       <!-- footer part start -->
       <footer class="py-3 my-4">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">View all Customers</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+          <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+          <li class="nav-item"><a href="customer.php" class="nav-link px-2 text-muted">View all Customers</a></li>
+          <li class="nav-item"><a href="index.php#About" class="nav-link px-2 text-muted">About</a></li>
         </ul>
         <p class="text-center text-muted">© 2021 Money Bank, Inc</p>
       </footer>
