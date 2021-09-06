4560b66bbb7d8f4fbd4eb311598bd3dccb058b2a
harish <harishkaiwart127@gmail.com>
2021-09-06 22:45:37 +0530

    failing to implement transection

--- connection.php
+++ connection.php
@@ -10,4 +10,15 @@ if(!$conn){
     die("Sorry we failed to connect:". mysqli_connect_error());
 }
 
+function check_name($from,$to,$amount){
+    // $sql = "SELECT Amount FROM `customers` where Name = '".$from."'  ";
+    // $result = mysqli_query($conn,$sql);
+    // $row = mysqli_fetch_assoc($result);
+    // $cur_amt = $row['Amount'];
+    // $dbh = new PDO('mysql:host=localhost;dbname=bank', 'root', '');
+    // $sth = $dbh->query('SELECT * FROM customer');
+    // //$sth = mysqli_query($conn,'SELECT * FROM foo');
+    // var_dump($sth);
+}
+
 ?>
