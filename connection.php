e7824ea8a047b96bb856cd926427a22e3f866b82
harish <harishkaiwart127@gmail.com>
2021-09-08 00:13:16 +0530

    unable to fix transaction

--- connection.php
+++ connection.php
@@ -11,14 +11,54 @@ if(!$conn){
 }
 
 function check_name($from,$to,$amount){
-    // $sql = "SELECT Amount FROM `customers` where Name = '".$from."'  ";
-    // $result = mysqli_query($conn,$sql);
-    // $row = mysqli_fetch_assoc($result);
-    // $cur_amt = $row['Amount'];
-    // $dbh = new PDO('mysql:host=localhost;dbname=bank', 'root', '');
-    // $sth = $dbh->query('SELECT * FROM customer');
-    // //$sth = mysqli_query($conn,'SELECT * FROM foo');
-    // var_dump($sth);
+
+    try{
+    // $db_name = 'mysql:host=localhost;dbname=bank';
+    // $conn = new PDO($db_name,'root','');
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $conn->beginTransaction();
+    // // $sql1 = $conn->prepare("UPDATE customers SET Amount = Amount - ? WHERE Name = ?");
+    // $sql1 = $conn->prepare("UPDATE `customers` SET `Amount`= `Amount`+ :amt WHERE `Name` = :fr");
+    // $sql2 = $conn->prepare("UPDATE customers SET Amount = Amount + ? WHERE Name = ?");
+    // $sql1->execute(array(':amt'=>(float)$amount,'fr'=>$from));
+    // $sql2->execute([(float)$amount,$to]);
+
+    $mysqli = new mysqli("localhost", "root", "", "bank");
+    $mysqli->set_charset("utf8mb4");
+    $mysqli->autocommit(FALSE);
+    $stmt = $mysqli->prepare("UPDATE customers SET Amount = Amount + 500 ");
+// $stmt->bind_param('s', $to);
+$amount = (float)$amount;
+var_dump($stmt->execute());
+$stmt->close();
+
+            echo "<h1>sucessfully</h1>";
+            var_dump((float)$amount);
+
+
+    }
+    catch(Exception $e){
+        $con1->rollback();
+        echo $e->getMessage();
+    }
 }
+
+// try {
+//     $mysqli->autocommit(FALSE); //turn on transactions
+//     $stmt1 = $mysqli->prepare("INSERT INTO myTable (name, age) VALUES (?, ?)");
+//     $stmt2 = $mysqli->prepare("UPDATE myTable SET name = ? WHERE id = ?");
+//     $stmt1->bind_param("si", $_POST['name'], $_POST['age']);
+//     $stmt2->bind_param("si", $_POST['name'], $_SESSION['id']);
+//     $stmt1->execute();
+//     $stmt2->execute();
+//     $stmt1->close();
+//     $stmt2->close();
+//     $mysqli->autocommit(TRUE); //turn off transactions + commit queued queries
+//   } catch(Exception $e) {
+//     $mysqli->rollback(); //remove all queries from queue if error (undo)
+//     throw $e;
+//   }
+  
+
 
 ?>
