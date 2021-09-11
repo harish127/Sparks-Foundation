cead1c61b8e96f8aa762af6f970480b28fb9958c
harish <harishkaiwart127@gmail.com>
2021-09-11 12:58:45 +0530

    added transaction page

---
+++ transaction.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- external css bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Money Bank</title>
+</head>
+<body>
+
+  <!-- header with connection and sql code -->
+  <?php require_once './include/header.php' ?>
+
+  
+
+           <!-- footer  -->
+  <?php require_once './include/footer.php' ?>  
+</body>
+
+      <!-- bootstrap js script -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</html>
